00b2c121d69da7edfa902d8559839d121f7106a9
Victory Quiah <vquiah@hotmail.com>
2022-06-18 12:56:00 -0700

    .

--- extractDomainFromURL.js
+++ extractDomainFromURL.js
@@ -13,3 +13,5 @@ function domainName(url){
     let sourceString = url.replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0];
      let domain = sourceString.split(".")[0];
      return domain;
+
+}
