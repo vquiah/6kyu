dcfbb35a11e1b3794ce601902083b86e1c937d73
Victory Quiah <vquiah@hotmail.com>
2022-07-17 12:56:00 -0700

    .

--- isTheNumPrime.js
+++ isTheNumPrime.js
@@ -4,7 +4,7 @@
 // Per Wikipedia, a prime number ( or a prime ) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
 
 // Requirements
-// You can assume you will be given an integer input.
+// You can assume you will be given an integer input
 // You can not assume that the integer will be only positive. You may be given negative numbers as well ( or 0 ).
 // NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 ( or similar, depending on language ). Looping all the way up to n, or n/2, will be too slow.
 // Example
