e8f60e264159e01ce0b87b77e88588b4b32f5bc7
Victory Quiah <vquiah@hotmail.com>
2022-07-28 09:35:14 -0700

    .

---
+++ splitStrings.js
@@ -0,0 +1,16 @@
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+// Examples:
+
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+
+function solution(str){
+    arr = [];
+    for(var i = 0; i < str.length; i += 2){
+      second = str[i+1] || '_';
+      arr.push(str[i] + second);
+    }
+    return arr;
+  }
