8a19e4482a00e253bae581cb838646db20a60e08
Victory Quiah <vquiah@hotmail.com>
2022-09-05 12:56:00 -0700

    .

--- tortoiseRacing.js
+++ tortoiseRacing.js
@@ -27,11 +27,8 @@
 
 
 function race (v1, v2, g) {
-
     if (v1 >= v2) return null
-  
     const time = g / ( v2 - v1 )
-  
     let hours = Math.floor(time)
     let minutes = Math.floor((time * 60) % 60)
     let seconds = Math.floor((time * 3600) % 60)
