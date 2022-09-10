e43ec809242027f3c39ff9cb937c1db3bf4c87c8
Victory Quiah <vquiah@hotmail.com>
2022-09-10 14:13:40 -0700

    .

---
+++ numbersInStrings.js
@@ -0,0 +1,28 @@
+// In this Kata, you will be given a string that has lowercase letters and numbers. Your task is to compare the number groupings and return the largest number. Numbers will not have leading zeros.
+
+// For example, solve("gh12cdy695m1") = 695, because this is the largest of all number groupings.
+
+// Good luck!
+
+
+function solve(s){
+    return Math.max(...s.match(/\d+/g));
+  };
+  
+  /*
+  input: num and letter..str
+  output: num
+  task: given a str of mixed letters and nums, return the largest num grouping
+  rules/req: 
+    -return the largest grouping num
+    -no leading zeros
+    -
+  question: 
+    -will there be an empty str? no
+    -
+  example: 
+    -solve("gh12cdy695m1") = 695
+    
+  psuedocode:
+    
+  */ 
