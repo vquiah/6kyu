af7dfbdb550cca6f51ba4bc14b75a84211574792
Victory Quiah <vquiah@hotmail.com>
2022-09-25 15:31:23 -0700

    .

--- codingMeetup3.js
+++ codingMeetup3.js
@@ -17,3 +17,34 @@
 //  Notes:
  
 //  The input array will always be valid and formatted as in the example above.
+
+
+function isRubyComing(list) {
+    let res = list.some(a=>a.language == 'Ruby')
+    if(res){
+      return true
+    }else{
+      return false
+    }
+    }
+  
+  
+  /*
+  input: array of objects
+  output: boolean
+  goal: return true if one Ruby dev signed up and false if no ruby dev signed up
+  rules/constraits: inout array will always be valid
+  questions:
+  example: 
+    var list1 = [
+    { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, language: 'Ruby' },
+    { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' },
+    { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' }
+  ];
+  will return true
+  
+  psuedocode:
+    filter thru the array of object
+    use some() method
+  
+  */ 
