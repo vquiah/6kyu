ca3c06a18153a731cfd8091fbd15c8fdad0701c4
Victory Quiah <vquiah@hotmail.com>
2022-10-27 22:19:24 -0700

    .

--- reurnTheMissingElement.js
+++ reurnTheMissingElement.js
@@ -21,9 +21,9 @@ function getMissingElement(superImportantArray){
     return [0,1,2,3,4,5,6,7,8,9].filter(function(n) {
       return superImportantArray.indexOf(n) === -1;
     })[0];
-  }
+  } 
 
   function getMissingElement(a){
     for(let i=0;i<10;i++)
       if(!a.includes(i)) return i;
-  }
+  } 
