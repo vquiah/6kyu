fbb0bd943a8999cbe9c2e08dab13102eeabd7264
Victory Quiah <vquiah@hotmail.com>
2022-11-03 19:31:07 -0700

    .

---
+++ commaDelimiters.js
@@ -0,0 +1,13 @@
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+
+function printArray(array){
+    console.log(array)
+      let a = array.join()
+      console.log(a)
+      return a
+      }
