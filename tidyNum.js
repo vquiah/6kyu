1bd0ef7ca537375d551145c9025fe30b150cac1f
Victory Quiah <vquiah@hotmail.com>
2022-11-07 12:00:00 -0700

    .

---
+++ tidyNum.js
@@ -0,0 +1,18 @@
+// Definition
+// A Tidy number is a number whose digits are in non-decreasing order.
+
+// Task
+// Given a number, Find if it is Tidy or not .
+
+const tidyNumber = (n, str = [...String(n)].sort((a,b)=> a - b).join('')) => str === String(n)
+
+
+or 
+
+const tidyNumber = n => {
+    let s = n.toString();
+    for (let i = 0; i < s.length-1; i++) {
+      if (s[i] > s[i+1]) return false;
+    }
+    return true;
+  }
