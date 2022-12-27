b7272cbcba9febde0271188a683afc45ef6f31a2
Victory Quiah <vquiah@hotmail.com>
2022-12-27 00:22:51 -0700

    message

--- lengthOfMissingArray.js
+++ lengthOfMissingArray.js
@@ -19,7 +19,8 @@ function getLengthOfMissingArray(arrArr) {
    if(result.includes(0)){
     return 0
    }
-   for (let i = 0; i<result.length; i++){
+   let i;
+   for ( i = 0; i<result.length; i++){
     if(result[i+1] != result[i]+1){
         return result[i]+1
     }
